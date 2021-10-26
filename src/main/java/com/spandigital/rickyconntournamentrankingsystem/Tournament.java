69a0af347bad651a8fec9176357e962de626fa6f
Ricky-Conn <rickysconn@gmail.com>
2021-10-26 18:38:44 +0200

    Printing in correct order (formatting issue)

--- src/main/java/com/spandigital/rickyconntournamentrankingsystem/Tournament.java
+++ src/main/java/com/spandigital/rickyconntournamentrankingsystem/Tournament.java
@@ -3,18 +3,19 @@ package com.spandigital.rickyconntournamentrankingsystem;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
 
 public class Tournament
 {
-    HashMap<String, Integer> teams;
+    private HashMap<String, Integer> teams = new HashMap<>();
 
     public Tournament()
     {
-        teams = new HashMap<>();
     }
 
     public String judgeMatch(String match)
@@ -29,15 +30,17 @@ public class Tournament
             String teamTwoName = getTeamName(scores[1]);
             int teamTwoScore = getTeamScore(scores[1]);
 
-            if(teamOneScore > teamTwoScore)
+            if (teamOneScore > teamTwoScore)
             {
                 addToPoints(teamOneName, 3);
                 addToPoints(teamTwoName, 0);
-            }else if(teamOneScore < teamTwoScore)
+            }
+            else if (teamOneScore < teamTwoScore)
             {
                 addToPoints(teamOneName, 0);
                 addToPoints(teamTwoName, 3);
-            }else
+            }
+            else
             {
                 addToPoints(teamOneName, 1);
                 addToPoints(teamTwoName, 1);
@@ -60,14 +63,15 @@ public class Tournament
     {
         return Integer.parseInt(teamResult.substring(teamResult.lastIndexOf(" ") + 1));
     }
-    
+
     //Adds points to team. If the team doesn't exist in teams then adds the team.
     public void addToPoints(String teamName, int points)
     {
-        if(teams.get(teamName) == null)
+        if (teams.get(teamName) == null)
         {
             teams.put(teamName, points);
-        }else
+        }
+        else
         {
             teams.put(teamName, teams.get(teamName) + points);
         }
@@ -86,12 +90,44 @@ public class Tournament
                 judgeMatch(match);
                 match = reader.readLine();
             }
-            
-            System.out.println(teams);
+
+            printTeams();
         }
         catch (Exception ex)
         {
             System.out.println("Exception: " + ex);
+        }
+    }
+
+    public void printTeams()
+    {
+        List<Entry<String, Integer>> teamsList = new LinkedList<Entry<String, Integer>>(teams.entrySet());
+
+        Collections.sort(teamsList, new Comparator<Entry<String, Integer>>()
+        {
+            public int compare(Entry<String, Integer> o1,
+                    Entry<String, Integer> o2)
+            {
+                if (o1.getValue() > o2.getValue())
+                {
+                    return -1;
+                }
+                else if (o1.getValue() < o2.getValue())
+                {
+                    return 1;
+
+                }
+                else
+                {
+                    return o1.getKey().compareToIgnoreCase(o2.getKey());
+                }
+            }
+        });
+
+        for (int i = 1; i <= teamsList.size(); i++)
+        {
+            Entry<String, Integer> team = teamsList.get(i - 1);
+            System.out.println(i + ". " + team.getKey() + ", " + team.getValue() + " pts");
         }
     }
 }
