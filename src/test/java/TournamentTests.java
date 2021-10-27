0f0e2aab15048e4cf512c000e362f43a8aac7f78
Ricky-Conn <rickysconn@gmail.com>
2021-10-27 10:44:18 +0200

    Created method to read expected output from file to compare with actual output

--- src/test/java/TournamentTests.java
+++ src/test/java/TournamentTests.java
@@ -1,4 +1,8 @@
 import com.spandigital.rickyconntournamentrankingsystem.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,5 +13,43 @@ public class TournamentTests
     {
         Tournament tournament = new Tournament("manySamePoints.txt");
         Assert.assertEquals(tournament.getResults(), "1. Ben, 1 pts\n2. FC Awesome, 1 pts\n3. Grouches, 1 pts\n4. Lions, 1 pts\n5. Snakes, 1 pts\n6. Tarantulas, 1 pts");
+    }
+    
+    
+    @Test
+    public void alphabeticalOrderNotAllTiesTest()
+    {
+        System.out.println(readFileWithNewlines("expectedManySamePoints.txt"));
+        Tournament tournament = new Tournament("manySamePoints.txt");
+//        Assert.assertEquals(tournament.getResults(), "1. Ben, 1 pts\n2. FC Awesome, 1 pts\n3. Grouches, 1 pts\n4. Lions, 1 pts\n5. Snakes, 1 pts\n6. Tarantulas, 1 pts");
+        Assert.assertEquals(tournament.getResults(), readFileWithNewlines("expectedManySamePoints.txt"));
+    }
+    
+    public String readFileWithNewlines(String filename)
+    {
+        StringBuilder expectedOutput = new StringBuilder();
+        try
+        {
+            String fullFilePath = System.getProperty("user.dir") + File.separator + filename;
+            BufferedReader reader = new BufferedReader(new FileReader(fullFilePath));
+            String expectedMatch = reader.readLine();
+
+            while (expectedMatch != null)
+            {
+                expectedOutput.append(expectedMatch);
+                expectedOutput.append("\n");
+                expectedMatch = reader.readLine();
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Exception: " + ex);
+        }
+        String results = expectedOutput.toString();
+        if(results.contains("\n"))
+        {
+            results = results.substring(0, results.lastIndexOf("\n"));
+        }
+        return results;
     }
 }
